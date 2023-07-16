e6ce82234d980cea220c22aef50a8b7e53f69de4
cqjtyyds <123456987ytw>
2023-07-16 13:50:21 +0800

    提交代码

---
+++ 7-9学习/7-9学习/FileName.cpp
@@ -0,0 +1,193 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<string.h>
+//#define max(a,b) (a>b?a:b)
+//int num1 = 13;
+//int num2 = 15;
+//int sum;
+//int add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//#define max(a,b) (a>b?a:b)
+int   main()
+{
+	int a = 10;
+	int b = 24;
+	int c = 2341;
+
+
+
+
+
+
+
+
+
+	/*char arr1[] = "welcome to bite!!!!!!";
+	char arr2[] = "#####################";
+	int left = 0;
+	int right = strlen(arr1) - 1;
+	while (left <= right)
+	{
+		
+		
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		printf("%s\n", arr2);
+		left++;
+		right--;
+	}*/
+	 
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//char Y = { 0 };
+	//char rec = { 0 };
+	//int password[20] = { 0 };
+	//printf("请输入密码：");
+	//scanf("%d", &password);
+	//getchar();
+	//printf("请确认（Y/N)");
+	//rec = getchar();
+	//
+	//if ('Y' == rec)
+	//{
+	//	printf("成功");
+
+	//}
+	//else
+	//{
+	//	printf("失败");
+	//}
+
+
+	/*int ch = 0;
+	while ((ch = getchar()) != EOF)
+	{
+		if (ch < '0' || ch >'9')
+			continue;
+
+		putchar(ch);
+	}*/
+	/*int temp = 1; 
+	int n=0;
+	int x = 0;
+	printf("shuru n\n");
+	scanf("%d", &n);
+	for (int i = 1; i < n+1; n++)
+	{
+		temp = 1;
+		for (int j = 1; j < n + 1; j++)
+		{
+			temp *= j;
+		}
+		 x =x+temp;
+
+	}
+	printf("shuch = %d", x);*/
+
+	//int arr[]={ 1,2,3,4,5,6,7,8,9,10 };
+	//int lift = 0;
+	//int k = 11 ;
+	//int sz = sizeof(arr) / sizeof(arr[0]);
+	//int right = sz - 1;
+	//
+	//while (lift <= right)
+	//{
+	//	int mid = (lift + right) / 2;
+ //       if (arr[mid] > k)
+	//	{
+	//		right = mid - 1;
+	//	}
+	//	else if (arr[mid] < k)
+	//	{
+	//		lift = mid + 1;
+	//	}
+	//	else
+	//	{
+	//		printf("找到了：%d", mid);
+	//		break;
+	//	}
+
+	//}
+	//if (lift > right)
+	//{
+	//	printf("找不到");
+	//}
+
+
+	/*int i=1;
+	while (i < 101)
+	{
+		i++;
+		if (i == 5)
+			continue;
+			printf("i = %d\n", i);
+		
+			i++;
+	}*/
+
+	/*struct book
+	{
+		char name[10];
+		int price;
+
+	};
+
+	struct book a = { "一本书",110 };
+	struct book* s = &a;
+	printf("指针炒作\n书名%s\n价格%d\n", s->name, s->price);
+	printf("name = %s\njiage = %d\n", a.name, a.price);*/
+
+	/*int xx = max(num1, num2);
+	printf("xx = %d\n", xx);*/
+
+	 
+	//int line = 0;
+	//printf("加入比特\n");
+	//while(line < 20000)
+	//{
+	//	printf("xieyihangdaima: %d\n",line);
+	//	line++;
+	//}
+	//
+	//if (line > 3000)
+	//{
+	//	printf("hao offer\n");
+	//}
+	/*scanf("%d%d", &num1, &num2);
+	sum = add(num1, num2);
+	printf("sum = : %d",sum);*/
+	
+	/*int arr[4] = { 1,2,3,4, };
+	int i = 0;
+	while (i < 4)
+	{
+		printf("%d\n", arr[i]);
+		i++;
+	}
+	*/
+	/*int a = 23;
+	int b = 15;
+	int max = 0;
+	max = (a > b ? a : b);
+	printf("max = %d", max);*/
+	/*int arr[4] = { 1,2,3,4, };
+	printf("arr = %d ", arr[2]);*/
+
+	
+	return 0;
+
+}
